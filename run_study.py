bc9dde89c2759a06da222ec1325044be1110cf0e
Neal Morton <mortonne@gmail.com>
2026-03-14 11:23:36 -0500

    feat: support multiple study runs and changing run number

--- run_study.py
+++ run_study.py
@@ -13,12 +13,18 @@ design_dir = Path("~/VSCode/operators/design/simulation").expanduser()
 env_files = ['./envs/2x3_env1.json', './envs/2x3_env2.json']
 subjects = ["001", "002"]
 conditions = {"001": "PI", "002": "AL"}
+n_runs = 100
+
+study_dir = design_dir / "study-1"
+while study_dir.exists():
+    number = int(study_dir.name.split("-")[1])
+    study_dir = design_dir / f"study-{number + 1}"
+study_dir.mkdir()
 
 for subject in subjects:
     cond = conditions[subject]
     design_files = [design_dir / f"sub-{subject}_graph-{graph}_design.csv" for graph in [1, 2]]
-    for run in range(1, 11):
-        out_dir = design_dir / f"cond-{cond}_run-{run:02d}"
-        out_dir.mkdir(exist_ok=True)
-        params = parameters.parameters()
-        tem_model = world.learn_design(params, env_files, design_files, out_dir)
+    out_dir = study_dir / f"cond-{cond}"
+    out_dir.mkdir(exist_ok=True)
+    for run in range(1, n_runs + 1):
+        tem_model = world.learn_design(env_files, design_files, out_dir, run)
