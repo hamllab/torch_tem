4a303da0ac2fbdccab82880e14dc844bd8c9a2f8
Linzan Liu <linzanliu@gmail.com>
2026-07-02 14:15:56 -0500

    refactor: change training parameter combinations for study-21

--- commands.sh
+++ commands.sh
@@ -1,25 +1,58 @@
-uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.1 --study-name study-grid-l0_9-e0_1   --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.3 --study-name study-grid-l0_9-e0_3   --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.5 --study-name study-grid-l0_9-e0_5   --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.7 --study-name study-grid-l0_9-e0_7   --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.9 --study-name study-grid-l0_9-e0_9   --n-subj 10
-uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.1 --study-name study-grid-l0_95-e0_1  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.3 --study-name study-grid-l0_95-e0_3  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.5 --study-name study-grid-l0_95-e0_5  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.7 --study-name study-grid-l0_95-e0_7  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.9 --study-name study-grid-l0_95-e0_9  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.1 --study-name study-grid-l0_99-e0_1  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.3 --study-name study-grid-l0_99-e0_3  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.5 --study-name study-grid-l0_99-e0_5  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.7 --study-name study-grid-l0_99-e0_7  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.9 --study-name study-grid-l0_99-e0_9  --n-subj 10
-uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.1 --study-name study-grid-l0_999-e0_1 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.3 --study-name study-grid-l0_999-e0_3 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.5 --study-name study-grid-l0_999-e0_5 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.7 --study-name study-grid-l0_999-e0_7 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.9 --study-name study-grid-l0_999-e0_9 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.1 --study-name study-grid-l0_9999-e0_1 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.3 --study-name study-grid-l0_9999-e0_3 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.5 --study-name study-grid-l0_9999-e0_5 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.7 --study-name study-grid-l0_9999-e0_7 --n-subj 10
-uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.9 --study-name study-grid-l0_9999-e0_9 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.05 --study-name study-grid-l0_99-e0_05 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.1 --study-name study-grid-l0_99-e0_1 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.15 --study-name study-grid-l0_99-e0_15 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.2 --study-name study-grid-l0_99-e0_2 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.25 --study-name study-grid-l0_99-e0_25 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.3 --study-name study-grid-l0_99-e0_3 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.35 --study-name study-grid-l0_99-e0_35 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.4 --study-name study-grid-l0_99-e0_4 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.45 --study-name study-grid-l0_99-e0_45 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.99 --eta-val 0.5 --study-name study-grid-l0_99-e0_5 --n-subj 10
+
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.05 --study-name study-grid-l0_999-e0_05 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.1 --study-name study-grid-l0_999-e0_1 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.15 --study-name study-grid-l0_999-e0_15 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.2 --study-name study-grid-l0_999-e0_2 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.25 --study-name study-grid-l0_999-e0_25 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.3 --study-name study-grid-l0_999-e0_3 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.35 --study-name study-grid-l0_999-e0_35 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.4 --study-name study-grid-l0_999-e0_4 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.45 --study-name study-grid-l0_999-e0_45 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.999 --eta-val 0.5 --study-name study-grid-l0_999-e0_5 --n-subj 10
+
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.05 --study-name study-grid-l0_9999-e0_05 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.1 --study-name study-grid-l0_9999-e0_1 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.15 --study-name study-grid-l0_9999-e0_15 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.2 --study-name study-grid-l0_9999-e0_2 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.25 --study-name study-grid-l0_9999-e0_25 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.3 --study-name study-grid-l0_9999-e0_3 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.35 --study-name study-grid-l0_9999-e0_35 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.4 --study-name study-grid-l0_9999-e0_4 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.45 --study-name study-grid-l0_9999-e0_45 --n-subj 10
+uv run jupyter/train.py -- lambda-val 0.9999 --eta-val 0.5 --study-name study-grid-l0_9999-e0_5 --n-subj 10
+
+#uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.1 --study-name study-grid-l0_9-e0_1   --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.3 --study-name study-grid-l0_9-e0_3   --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.5 --study-name study-grid-l0_9-e0_5   --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.7 --study-name study-grid-l0_9-e0_7   --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9    --eta-val 0.9 --study-name study-grid-l0_9-e0_9   --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.1 --study-name study-grid-l0_95-e0_1  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.3 --study-name study-grid-l0_95-e0_3  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.5 --study-name study-grid-l0_95-e0_5  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.7 --study-name study-grid-l0_95-e0_7  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.95   --eta-val 0.9 --study-name study-grid-l0_95-e0_9  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.1 --study-name study-grid-l0_99-e0_1  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.3 --study-name study-grid-l0_99-e0_3  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.5 --study-name study-grid-l0_99-e0_5  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.7 --study-name study-grid-l0_99-e0_7  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.99   --eta-val 0.9 --study-name study-grid-l0_99-e0_9  --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.1 --study-name study-grid-l0_999-e0_1 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.3 --study-name study-grid-l0_999-e0_3 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.5 --study-name study-grid-l0_999-e0_5 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.7 --study-name study-grid-l0_999-e0_7 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.999  --eta-val 0.9 --study-name study-grid-l0_999-e0_9 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.1 --study-name study-grid-l0_9999-e0_1 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.3 --study-name study-grid-l0_9999-e0_3 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.5 --study-name study-grid-l0_9999-e0_5 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.7 --study-name study-grid-l0_9999-e0_7 --n-subj 10
+#uv run jupyter/train.py --lambda-val 0.9999 --eta-val 0.9 --study-name study-grid-l0_9999-e0_9 --n-subj 10
